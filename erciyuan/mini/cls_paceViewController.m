2e1ac2f91f0b0912f7452b5b35889629ce3773ac
Kratos28 <c193293@126.com>
2024-03-09 18:25:49 +0800

    ggg

--- erciyuan/mini/cls_paceViewController.m
+++ erciyuan/mini/cls_paceViewController.m
@@ -8,6 +8,7 @@
 #import "cls_paceViewController.h"
 
 @interface cls_paceViewController ()
+@property (nonatomic,copy)NSArray *pro_dicst;
 
 @end
 
@@ -50,7 +51,61 @@
         }
     }
     
+    int  corlmax = 3;
+
+    int itemW =  RPX(250);
+    int itemH =  RPY(200);
+    int mariginX = RPX(50);
+    int startY = RPY(180);
+    int mariginH =  RPY(60);
     
+    CGFloat const totalWidth = corlmax * itemW + (corlmax - 1) * mariginX;
+    CGFloat const startX = RPX(100);
+    
+
+    
+    
+    NSArray *array =    @[
+        @{@"name":@"medicine",@"gold":@"鞋子",@"num":@1,@"jinbi":@60,@"price":@6,@"id":@"com.sxsn.zs6"},
+    @{@"name":@"Skill",@"gold":@"yifu",@"num":@5,@"jinbi":@300,@"price":@30,@"id":@"com.sxsn.zs30"},
+    @{@"name":@"Breakthrough",@"gold":@"mofabang",@"num":@10,@"jinbi":@680,@"price":@68,@"id":@"com.sxsn.zs68"},
+    @{@"name":@"Dog",@"gold":@"shoutao",@"num":@1,@"jinbi":@1280,@"price":@128,@"id":@"com.sxsn.zs128"},
+    @{@"name":@"birdie",@"gold":@"nengliangping",@"num":@1,@"jinbi":@1980,@"price":@198,@"id":@"com.sxsn.zs198"},
+    @{@"name":@"birdie",@"gold":@"zhouyu",@"num":@1,@"jinbi":@3280,@"price":@320,@"id":@"com.sxsn.zs328"}
+    ];
+//    sel
+    self.pro_dicst = array;
+    for (int i = 0; i < array.count; i++)
+    {
+        int row = i / corlmax;
+        int col = i % corlmax;
+        UIButton *b = [[UIButton alloc]init];
+        [var_imageview addSubview:b];
+        [b setTitle:@"" forState:UIControlStateNormal];
+        
+        NSDictionary *ji = array[i];
+        UIButton *fji = [[UIButton alloc]init];
+        [fji setBackgroundImage:[cls_Tool fun_imageName:ji[@"gold"]] forState:0];
+        [b addSubview:fji];
+        fji.frame = CGRectMake(RPX(70), RPY(50), RPX(100), RPY(100));
+        
+        
+        NSDictionary *dict =  array[i];
+        [b setBackgroundImage:[cls_Tool fun_imageName:@"zhuangbeikuang"] forState:UIControlStateNormal];
+        b.titleLabel.font = [UIFont boldSystemFontOfSize:([UIScreen mainScreen].bounds.size.width * 0.1)];
+        b.backgroundColor = [UIColor redColor];
+        b.titleLabel.textAlignment = NSTextAlignmentRight;
+        b.tag = i;
+        b.translatesAutoresizingMaskIntoConstraints = NO;
+        [b.widthAnchor constraintEqualToConstant:itemW].active = YES;
+        [b.heightAnchor constraintEqualToConstant:itemH].active = YES;
+        [b.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:startX + col * (mariginX + itemW)].active = YES;
+        [b.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:startY + row * (mariginH + itemH)].active = YES;
+        
+        
+        [b addTarget:self action:@selector(fun_click:) forControlEvents:64];
+        
+    }
 }
 
 
